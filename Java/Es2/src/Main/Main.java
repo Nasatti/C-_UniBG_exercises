bd4b0f2630d66501e4b83f128efa42f0f774f826
RiccardoNasatti <riccardonasatti@gmail.com>
2024-03-07 13:12:53 +0100

    java

--- Java/Es2/src/Main/Main.java
+++ Java/Es2/src/Main/Main.java
@@ -1,11 +1,16 @@
 package Main;
 
 import Es2.Bike;
+import java.util.Scanner;
+import java.awt.Color;
 
 public class Main {
 	
 	public static void main(String[] args) {
-		int x = 5;
+		
+		Scanner sc = new Scanner(System.in);
+		sc.nextLine(); 
+		
 		Bike b;//creates una reference
 		b=new Bike(); //creates a new bike
 		
@@ -16,6 +21,12 @@ public class Main {
 		Bike b3=new Bike(2,0,0);
 		b3.gear = 5;
 		
+		Bike b4 = new Bike(Color.BLUE);
 		
+		Bike[] bikes = new Bike[10];
+		bikes[0]=new Bike(Color.RED);
+		bikes[0]=new Bike(Color.GREEN);
+		
+		bikes[0].getGear();
 	}
 }
